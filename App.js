87d60dc67e0488d669492b37981ea1b47f21c816
mikias-worash <mikias.worash.a@gmail.com>
2023-01-18 00:25:43 +0300

    added text and goals states

--- App.js
+++ App.js
@@ -1,14 +1,51 @@
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native'
+import { useState } from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView,
+  FlatList,
+} from 'react-native'
 
 export default function App() {
+  const [text, setText] = useState('')
+  const [goals, setGoals] = useState([])
+
+  function goalInputHandler(enteredText) {
+    setText(enteredText)
+  }
+
+  function addGoalHandler() {
+    setGoals((currentGoals) => [
+      ...currentGoals,
+      { text: text, key: Math.random().toString() },
+    ])
+  }
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Your 2023 goals!" style={styles.textInput} />
-        <Button title="Add Goal" />
+        <TextInput
+          placeholder="Your 2023 goals!"
+          style={styles.textInput}
+          onChangeText={goalInputHandler}
+        />
+        <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
+
       <View style={styles.goalsContainer}>
-        <Text>List of goals..</Text>
+        <FlatList
+          data={goals}
+          renderItem={(itemData) => {
+            return (
+              <View style={styles.goalItem}>
+                <Text style={styles.goalText}>{itemData.item.text}</Text>
+              </View>
+            )
+          }}
+        />
       </View>
     </View>
   )
@@ -38,5 +75,14 @@ const styles = StyleSheet.create({
   },
   goalsContainer: {
     flex: 5,
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: '#5e0acc',
+  },
+  goalText: {
+    color: 'white',
   },
 })
